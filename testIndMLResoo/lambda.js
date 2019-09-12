de050fd38c825caa0dbe21bc526dd6be6d4b315d
Indunil1992 <indunil@adroitlogic.com>
2019-09-12 14:11:26 +0530

    j

--- testIndMLResoo/lambda.js
+++ testIndMLResoo/lambda.js
@@ -1,19 +1,19 @@
 let AWS = require('aws-sdk');
 const ml = new AWS.MachineLearning();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     ml.predict({
-    		MLModelId: 'ml-Gb8QvgzHy4K',
-    		PredictEndpoint: 'https://realtime.machinelearning.us-east-1.amazonaws.com',
-    		Record: {
-    	100:1992
-    		}
-    	}, function (err, data) {
-    		if (err) console.log(err, err.stack); 
-            // an error occurred
-    		else console.log(data);    
-            console.log("sucessss");    // successful response
-    	});
-    
-    callback(null, {"message": "Successfully executed"});
+        MLModelId: 'ml-Gb8QvgzHy4K',
+        PredictEndpoint: 'https://realtime.machinelearning.us-east-1.amazonaws.com',
+        Record: {
+            a: 1992
+        }
+    }, function (err, data) {
+        if (err) console.log(err, err.stack);
+        // an error occurred
+        else console.log(data);
+        console.log("sucessss");    // successful response
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
